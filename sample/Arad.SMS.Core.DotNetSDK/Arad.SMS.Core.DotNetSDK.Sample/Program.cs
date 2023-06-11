71992427615f39cfd8acccec222b500a83569c5d
Akbar Aslani <Akbar.Aslani@gmail.com>
2023-06-11 15:48:07 +0330

    Bug fix on sample

--- sample/Arad.SMS.Core.DotNetSDK/Arad.SMS.Core.DotNetSDK.Sample/Program.cs
+++ sample/Arad.SMS.Core.DotNetSDK/Arad.SMS.Core.DotNetSDK.Sample/Program.cs
@@ -16,22 +16,23 @@ namespace Arad.SMS.Core.DotNetSDK.Sample
     {
         private static IMessage _aradMessage;
 
-        private static void Main()
+        private static async Task Main()
         {
             Console.WriteLine("Hello Arad!");
             _aradMessage = new Message("https://api.aradvas.ir");
-            SendAradA2PMessage();
+            
+            await SendAradA2PMessage();
         }
 
-        private static async Task<string> GetToken()
+        private static async ValueTask<string> GetToken()
         {
-            return await _aradMessage.GetToken("UserName", "Password", "ApiAccess", CancellationToken.None);
+            return await _aradMessage.GetToken("userName", "password", "ApiAccess", CancellationToken.None);
         }
 
-        private static async void SendAradA2PMessage()
+        private static async ValueTask SendAradA2PMessage()
         {
             List<AradA2PMessage> list = new() { new() { DestinationAddress = "989123456789", SourceAddress = "9890001234", MessageText = "Hello Arad!", DataCoding = Enums.DataCodings.Default } };
-
+            
             HttpResponseMessage response = await _aradMessage.Send(list, await GetToken(), false, CancellationToken.None);
 
             if (response.StatusCode == HttpStatusCode.OK)
@@ -41,7 +42,7 @@ namespace Arad.SMS.Core.DotNetSDK.Sample
             }
         }
 
-        private static async void SendAradBulkMessage()
+        private static async ValueTask SendAradBulkMessage()
         {
             AradBulkMessage list = new() { DestinationAddress = { "989123456789", "989129876543" }, SourceAddress = "9890001234", MessageText = "Hello Arad!" };
             HttpResponseMessage response = await _aradMessage.SendBulk(list, await GetToken(), false, CancellationToken.None);
@@ -53,20 +54,20 @@ namespace Arad.SMS.Core.DotNetSDK.Sample
             }
         }
 
-        private static async void GetDelivery()
+        private static async ValueTask GetDelivery()
         {
             List<string> messageIds = new() { "64587abc831e4ec10a883dcd", "64587abc831e4ec10a883hgf" };
             List<DLRStatus> response = await _aradMessage.GetDLR(messageIds, await GetToken(), false, CancellationToken.None);
             Console.Write(JsonConvert.SerializeObject(response));
         }
 
-        private static async void GetMo()
+        private static async ValueTask GetMo()
         {
             List<Inbox> response = await _aradMessage.GetMO(false, await GetToken(), CancellationToken.None);
             Console.Write(JsonConvert.SerializeObject(response));
         }
 
-        private static async void GetMoByDate()
+        private static async ValueTask GetMoByDate()
         {
             List<Inbox> response = await _aradMessage.GetMOByDate(DateTime.Now.AddMinutes(-20), DateTime.Now, false, await GetToken(), CancellationToken.None);
             Console.Write(JsonConvert.SerializeObject(response));
